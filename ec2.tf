125b0dbc8d4c1315a332f9ed59a0d2956163d12e
Kreyno93 <michael.wagener@neuefische.de>
2023-07-21 10:21:12 +0200

    change instance_type

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0ae49954dfb447966"
-  instance_type           = "t3.micro"
+  instance_type           = "t2.micro"
   key_name                = "vockey"
   vpc_security_group_ids  = [aws_security_group.devVPC_sg_allow_ssh_http.id]
   subnet_id               = aws_subnet.devVPC_public_subnet.id
